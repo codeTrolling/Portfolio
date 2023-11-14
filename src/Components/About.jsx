6420d2cd3feadc9f3f44cd699ebd2e8f38d03cf2
Your name <wedoulilishte@gmail.com>
2023-11-14 22:30:28 +0200

    Beginning

---
+++ src/Components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div className="viewport flex-column">
+            <h1 className="theme-font-family">Welcome to my portfolio!</h1>
+            <p className="theme-font-family"></p>
+        </div>
+    )
+}
+
+export default About;
