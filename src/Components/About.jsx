7d36c71ce9a960310014f176dfa8549d851cf297
Your name <wedoulilishte@gmail.com>
2023-11-17 15:29:50 +0200

    Make components appear/disappear when on/off screen

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,24 @@
 import "../Styles/AboutStyles.css";
+import { useRef, useState } from "react";
 
 const About = () => {
+
+    //hide component if out of screen (and show it if it's in the screen)
+    const viewportRef = useRef();
+    const [isOnScreen, setIsOnScreen] = useState(true);
+    window.addEventListener("scroll", () => {
+        if(viewportRef.current !== undefined && (viewportRef.current.getBoundingClientRect().bottom <= 0 || viewportRef.current.getBoundingClientRect().top >= window.innerHeight)){
+            setIsOnScreen(false);
+        }
+        else{
+            setIsOnScreen(true);
+        }
+    })
+    //
+
+
     return(
-        <div className="viewport flex-column">
+        <div className="viewport flex-column" ref={viewportRef} style={{opacity: isOnScreen ? "1" : "0", transition: isOnScreen ? "all 0.6s cubic-bezier(0.25, 0.46, 0.45, 0.94)" : "all 1s linear", transform: isOnScreen ? "translateX(0)" : "translate(100px)"}}>
             <h1 className="theme-font-family heading">About</h1>
             <label className="theme-font-family" style={{fontSize: "2.1rem", alignSelf: "center"}}>Welcome to my portfolio!</label>
             <p className="theme-font-family">Hello! I am a student in computer science as well as an aspiring programmer. <br/> I like playing video games and sports and watching useless youtube videos.
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
 
     return(
         <div className="flex-column navigation-container" style={{transform: showNavigation ? "translateX(0)" : "translateX(400px)", borderRadius: borderRadiuses, transition: showNavigation ? "transform 0.3s ease-in-out, border-radius 0.6s ease-out" : "transform 0.3s ease-in-out, border-radius 0.6s ease-in"}}>
-            <button className="show-hide-navigation-btn" onClick={() => {setShowNavigation(!showNavigation); navigationBorders(); console.log(showNavigation)}}></button>
+            <button className="show-hide-navigation-btn" onClick={() => {setShowNavigation(!showNavigation); navigationBorders()}}></button>
             <label className="theme-font-family navigation-options">About</label>
             <label className="theme-font-family navigation-options">Skills</label>
             <label className="theme-font-family navigation-options">Beginning</label>
